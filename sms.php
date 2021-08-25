356360a032b48f5e66e30b0c9d96bd555e1c2fad
ZovoGeeks <89540338+zovoteamltd@users.noreply.github.com>
2021-08-25 23:46:27 +0600

    Update sms.php

--- sms.php
+++ sms.php
@@ -1,12 +1,12 @@
 <?php
-function notify_woosms_send_sms($mobilenumber, $smsbodytext){
-	$options = get_option( 'notify_woosms_settings' );
+function zovo_woosms_send_sms($mobilenumber, $smsbodytext){
+	$options = get_option( 'zovo_woosms_settings' );
 	$body = array(
     'recipient' => $mobilenumber,
-    'sender' => $options['notify_woosms_api_mask'],
+    'sender' => $options['zovo_woosms_api_mask'],
     'body' => $smsbodytext,
-    'userid' => $options['notify_woosms_api_user_name'],
-    'password' => $options['notify_woosms_api_password']
+    'userid' => $options['zovo_woosms_api_user_name'],
+    'password' => $options['zovo_woosms_api_password']
 	);
 
 	$args = array(
@@ -19,14 +19,14 @@ function notify_woosms_send_sms($mobilenumber, $smsbodytext){
 	    'cookies' => array()
 	);
 
-	if ($options['notify_woosms_select_provider'] == 'dianahost_psms') {
-		$response = wp_remote_post( 'https://psms.dianahost.com/api/sms/v1/send', $args );
-	} elseif ($options['notify_woosms_select_provider'] == 'dianahost_esms') {
-		$apikey = $options['notify_woosms_api_key'];
-		$response = wp_remote_post( 'http://esms.hostdokan.com/smsapi?api_key='.$apikey.'&type=text&contacts='.$mobilenumber.'&msg='.$smsbodytext.'&senderid='.$options['notify_woosms_api_mask'] );
-	} elseif ($options['notify_woosms_select_provider'] == 'dianahost_gsms') {
-		$apikey = $options['notify_woosms_api_key'];
-		$response = wp_remote_post( 'http://gsms.pw/smsapi?api_key='.$apikey.'&type=text&contacts='.$mobilenumber.'&msg='.$smsbodytext.'&senderid='.$options['notify_woosms_api_mask'] );
+	if ($options['zovo_woosms_select_provider'] == 'zovogeeks_psms') {
+		$response = wp_remote_post( 'http://sms.zovogeeks.com/api/sms/v1/send', $args );
+	} elseif ($options['zovo_woosms_select_provider'] == 'zovogeeks_esms') {
+		$apikey = $options['zovo_woosms_api_key'];
+		$response = wp_remote_post( 'http://sms.zovogeeks.com/smsapi?api_key='.$apikey.'&type=text&contacts='.$mobilenumber.'&msg='.$smsbodytext.'&senderid='.$options['zovo_woosms_api_mask'] );
+	} elseif ($options['zovo_woosms_select_provider'] == 'zovo_gsms') {
+		$apikey = $options['zovo_woosms_api_key'];
+		$response = wp_remote_post( 'http://sms.zovogeeks.com/smsapi?api_key='.$apikey.'&type=text&contacts='.$mobilenumber.'&msg='.$smsbodytext.'&senderid='.$options['zovo_woosms_api_mask'] );
 	}
 
 
